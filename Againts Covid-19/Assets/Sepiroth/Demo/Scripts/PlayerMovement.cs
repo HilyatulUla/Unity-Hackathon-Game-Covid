23ae971212dcbc423b6275ebb7e3d37d2a674001
HilyatulUla <62746451+HilyatulUla@users.noreply.github.com>
2020-03-30 23:27:36 +0700

    test

---
+++ Againts Covid-19/Assets/Sepiroth/Demo/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 80f;
+    float horizontalMove;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoundManager.PlaySound("music");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            SoundManager.PlaySound("jump");
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+        if (Input.GetButton("Crouch"))
+        {
+            crouch = true;
+        }
+        else
+        {
+            crouch = false; 
+        }
+
+
+        if (Input.touchCount > 0)
+        {
+                Touch touch = Input.GetTouch(0);
+
+                float direction = touch.position.x > Screen.width / 2 ? 1 : -1;
+                horizontalMove = direction * runSpeed;
+                animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+            if ((Input.touchCount > 1))
+            {
+                jump = true;
+                animator.SetBool("isJumping", true);
+            }
+            if ((touch.position.y < Screen.width / 8))
+            {
+                crouch = true;
+            }
+        }
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
+
+    }
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ Againts Covid-19/Assets/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip musicongame, playerHitSound, cleanSound, jumpSound, enemyDeathSound;
+    static AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //musicongame = Resources.Load<AudioClip>("music");
+        playerHitSound = Resources.Load<AudioClip>("playerHit");
+        cleanSound = Resources.Load<AudioClip>("clean");
+        jumpSound = Resources.Load<AudioClip>("jump");
+        enemyDeathSound = Resources.Load<AudioClip>("enemyDeath");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void PlaySound  (string clip)
+    {
+        //audioSrc.PlayOneShot(musicongame);
+        switch (clip)
+        {
+            case "clean":
+                audioSrc.PlayOneShot(cleanSound);
+                break;
+            case "playerHit":
+                audioSrc.PlayOneShot(playerHitSound);
+                break;
+            case "jump":
+                audioSrc.PlayOneShot(jumpSound);
+                break;
+            case "enemyDeath":
+                audioSrc.PlayOneShot(enemyDeathSound);
+                break;
+        }
+    }
+}
